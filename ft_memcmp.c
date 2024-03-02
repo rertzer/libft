90ca255af462fb5e27681ccfd2abc8c4883c4bfe
rertzer <rertzer@student.42.fr>
2024-03-02 11:12:07 +0100

    ft_memcmp.c refactoring

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: rertzer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 14:23:41 by rertzer           #+#    #+#             */
-/*   Updated: 2022/11/14 14:14:59 by rertzer          ###   ########.fr       */
+/*   Updated: 2024/03/02 11:10:22 by rertzer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,12 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 
 	if (n == 0)
 		return (0);
-	cs1 = s1;
-	cs2 = s2;
 	i = 0;
+	cs1 = (const unsigned char *)s1;
+	cs2 = (const unsigned char *)s2;
 	while (cs1[i] == cs2[i] && i < n - 1)
-		i++;
+	{
+		++i;
+	}
 	return (cs1[i] - cs2[i]);
 }
